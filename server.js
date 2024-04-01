2f3b1e5854bacf8891e1804e2ef6faa4df83b956
JHann <jhann@tamu.edu>
2024-04-01 13:04:17 -0500

    trying to fix crashing

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const app_folder = "./";
-
 //Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.all('*', function (req, res) {
-    res.status(200).sendFile(`/`, {root: app_folder});
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/quilt-blog'));
+
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/quilt-blog/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
