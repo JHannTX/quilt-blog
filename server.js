71dc19c7c0c5fa4609a3b0357c1a8d6bff68f50c
JHann <jhann@tamu.edu>
2024-04-01 12:51:24 -0500

    trying to fix crashing

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const app_folder = "./";
+
 //Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
+
+app.all('*', function (req, res) {
+    res.status(200).sendFile(`/`, {root: app_folder});
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
