0d3ac4c40a36a8edb8a12fba696b49fed5d412d9
JHann <jhann@tamu.edu>
2024-03-31 22:55:43 -0500

    adjusting styling further

--- src/app/progress/progress-home/progress-home.component.ts
+++ src/app/progress/progress-home/progress-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, type OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ProgressTitle } from 'src/page-content/models/progress-interfaces';
 import { ProgressService } from 'src/page-content/progress/progress.service';
@@ -11,9 +12,15 @@ import { ProgressService } from 'src/page-content/progress/progress.service';
 export class ProgressHomeComponent implements OnInit {
   titles$!: Observable<ProgressTitle[]>;
 
-  constructor(private progressService: ProgressService) {}
+  constructor(private progressService: ProgressService,
+    private router: Router,
+    private route: ActivatedRoute) {}
 
   ngOnInit(): void { 
     this.titles$ = this.progressService.titleList();
+  }
+
+  onClick(title: ProgressTitle) {
+    this.router.navigate([title.urlSegment], {relativeTo: this.route})
   }
 }
