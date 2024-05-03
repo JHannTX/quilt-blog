38dc1e2c93616bce71ef38e1bcf1d858628f6fdc
JHann <jhann@tamu.edu>
2024-05-03 18:55:34 -0500

    Adding the camel/sand quilt content

--- src/page-content/progress/progress-pages.ts
+++ src/page-content/progress/progress-pages.ts
@@ -3,22 +3,109 @@ import { Article } from "../models/article-interfaces"
 export const progressPages: Article[] = [
     {
         title: {
-            title: "derp",
-            subTitle: "testing derp",
-            urlSegment: "derp"
+            title: "Sand Quilt",
+            subTitle: "Featuring a Camel",
+            urlSegment: "camel"
         },
         sections: [
+            {
+                header: {
+                    header: "Inspiration"
+                },
+                content: [
+                    {
+                        paragraph: "This quilt started as wanting a way to practice adding a triangle to a square.  There is popular block that does this called snail trail or as I like to call it, the wave block.",
+                        picture: "https://i.imgur.com/rs8TXYN.png"
+                    },
+                    {
+                        paragraph: "So I decided to make this this block, but now I had to decide how.  Now it is typically used in ocean like quilts, but I had one I was planning already.  So I decided that I would make sand waves."
+                    },
+                    {
+                        paragraph: "The next step was to determine what to go with it.  I looked at various ideas like Cacti or other animals, but decided that this lovely camel would the center piece of the quilt.",
+                        picture: "https://i.imgur.com/qbmfuzQ.png"
+                    }, 
+                    {
+                        paragraph: "Now I had the camel and the surrounding wave blocks planned, but it still felt lacking.  I planned on adding a border on three sides with this cool mountain block.",
+                        picture: "https://i.imgur.com/9HD0tHu.png"
+                    },
+                    {
+                        paragraph: "This was the final result on my paper plan for the quilt.",
+                        picture: "PLACE HOLDING PICTURE"
+                    },
+                    {
+                        paragraph: "And since this was a sand quilt all the colors were in the warm color spectrum.  Trying to focus on yellow, orange, and pink.  Some red was used, but was supposed to mainly be in the camel."
+                    }
+                ]
+        },
         {
             header: {
-                header: "derp"
+                header: "Fabric Selection"
             },
             content: [
                 {
-                    paragraph: "derp",
-                    picture: "https://i.imgur.com/9jAkP7w.jpg"
+                    paragraph: "The fabric was from a variety of sources.  These included small bits from a shop, random purchases, and generally leftover fabric.  Very little was purchased just for this quilt."
                 },
                 {
-                    paragraph: "derp again"
+                    paragraph: "The fabric purchased for this quilt was all of the beige fabric.  I just didn't have much lying around and had to go out to get several different types."
+                },
+                {
+                    paragraph: "One of the fabrics I wanted to buy was a wideback and didn't realize it initially.  I decided to buy it anyways and use it almost every part of the quilt."
+                }
+            ]
+        },
+        {
+           header: {
+                header: "Construction"
+           },
+           content: [
+
+           ] 
+        },
+        {
+            header: {
+                header: "Back Design"
+            },
+            content: [
+
+            ]
+        },
+        {
+            header: {
+                header: "Back Construction"
+            },
+            content: [
+
+            ]
+        },
+        {
+            header: {
+                header: "Binding"
+            },
+            content: [
+                {
+                    paragraph: "My first quilt had four different fabrics put together to make the binding. After making several quilts with all the same fabric for the binding, I decided I do kinda like the random look to the binding.  For this quilt though I didn't do the exact same."
+                },
+                {
+                    paragraph: "The binding is made up of the beige wideback and random colors that I had enough of to cut a decent bit for the binding.  There are different lengths of beige with random colors inserted."
+                }
+            ]
+        },
+        {
+            header: {
+                header: "Quilting",
+                description: "Concept and Execution"
+            },
+            content: [
+                {
+                    paragraph: "When showing off the finished top at a quilt group show and tell, they suggested I do just general diagonals across.  While it was an interesting idea, it just didn't seem right.  This though helped spark the idea I initially settled on."
+                },
+                {
+                    paragraph: "It's initially just simple echo quilting, but with a slight twist.  I was going through my machine quilting pins and saw one featuring pop quilting.  Naturally I had to look further into this and decided this would be fun to try.",
+                    picture: "PLACEHOLDER POP QUILTING PIN"
+                },
+                {
+                    paragraph: "I then planned out, initially, what I thought would look nice.  This was done by just using the draw feature from my iPhone on an image of the quilt.",
+                    picture: "PLACEHOLDER FOR DRAWN ON PICTURE"
                 }
             ]
         }]
@@ -43,26 +130,5 @@ export const progressPages: Article[] = [
                 ]
             }
         ]
-    },
-    {
-        title: {
-            title: "Testing no subtitle",
-            urlSegment: "subtitle"
-        },
-        sections: [
-        {
-            header: {
-                header: "derp"
-            },
-            content: [
-                {
-                    paragraph: "derp",
-                    picture: "https://i.imgur.com/9jAkP7w.jpg"
-                },
-                {
-                    paragraph: "derp again"
-                }
-            ]
-        }]
     },
 ]
